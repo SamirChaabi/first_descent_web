91e1ed96f1b9280af738d93a073cc282a18c3aaf
Samir Chaabi <samir.chaabi@outlook.com>
2016-10-12 20:38:00 +0200

    Base func. to change lang added

--- js/app.js
+++ js/app.js
@@ -92,6 +92,44 @@ var main = function() {
     $("#fbicon").attr('src', "Images/contact/fb_black.png");
   });
 
+  var dictionary = {
+      'skinboard': {
+        'eng': 'Ski & Board Rental',
+        'ger': 'German Ski & Board Rental',
+        'fr': 'Salut',
+      },
+      'service': {
+        'eng': 'Service',
+        'ger': 'German Service',
+        'fr': 'Salut',
+      }
+  };
+
+  var langs = ['eng', 'ger', 'fr'];
+  var current_lang_index = 0;
+  var current_lang = langs[current_lang_index];
+
+  window.change_lang = function(id) {
+    if (id == "eng") {
+      current_lang = langs[0];
+    }
+    else if (id == "ger") {
+      current_lang = langs[1];
+    }
+    /*current_lang_index = ++current_lang_index % 3;
+    current_lang = langs[current_lang_index];*/
+    translate(id);
+  }
+
+  function translate(id) {
+    $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+  }
+
+  translate();
+
   $(".iframe").fancybox({
     'width'         : '90%',
     'height'        : '90%',
